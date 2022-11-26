7be3d5b9fe3099a759a523b9d1b850892a5bca57
wangyifeng <2665939276@qq.com>
2022-11-26 09:57:48 +0800

    feat: 文档库结构搭建

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,63 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around',
-  base:"/wjhs-doc/"
+  title: '网捷回收',
+  description: '网捷回收-项目开发文档',
+  base: "/wjhs-doc/",
+  theme: 'reco',
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: '/63763453.jpg',
+      },
+    ],
+  ],
+  themeConfig: {
+    // logo: ' /63763453.jpg',
+    lastUpdated: '最后更新时间',
+    sidebar: 'auto',
+    nav: [
+      {
+        text: '首页',
+        link: '/',
+      },
+      {
+        text: '服务端',
+        link: '/pages/server.md',
+      },
+      {
+        text: '平台端',
+        link: '/pages/platform.md',
+      },
+      {
+        text: 'APP',
+        link: '/pages/app.md',
+      },
+      {
+        text: '小程序',
+        link: '/pages/mp.md',
+      },
+      {
+        text: '项目资源地址',
+        items: [
+          {
+            text: '服务端',
+            link: '',
+          },
+          {
+            text: '平台端',
+            link: '',
+          },
+          {
+            text: 'APP',
+            link: '',
+          },
+          {
+            text: '小程序',
+            link: '',
+          },
+        ],
+      },
+    ],
+  },
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,16 @@
-### 网捷回收-项目开发文档
+---
+home: true
+heroImage: /63763453.jpg
+heroText: 'wjhs'
+tagline: 网捷回收-项目开发文档
+features:
+- title: 服务端技术栈
+  details: SpringBoot + mybatis + mysql + knife4j + flyway
+- title: 平台端技术栈
+  details: vue3 + typescript + axios + vite + pinia
+- title: APP技术栈
+  details: flutter + dio + getx + flutter_screenutil + provider + 原生android
+- title: 小程序技术栈
+  details: 原生小程序 + typescript
+footer: Copyright © ilovesshan
+---
